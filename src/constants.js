dd2f95dfc5f973e8ef468d0018f5c2f0db733ef3
Oleksii <klymoleksii12@proton.me>
2026-06-29 18:23:53 +0200

    modificated secciont

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 export const SECTIONS = [
+    '2401 Almacen GENERAL M.G.F',
     '2601 Taller Matadero de Cerdos',
     '2701 Taller Diespiece de Cerdos',
+    '2703 Envasados Industrial',
     '2704 Taller P.L.S',
   ];
